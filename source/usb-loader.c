54e09bcafaa2a4572ae2d1bf53e1515113daa458
wiiNinjaOne <wiiNinjaOne@c00eb1dc-4073-11de-af2b-f15df6ab9c44>
2009-07-13 15:57:19 +0000

    Added support for cIOS222/223. Added menu items and support for cIOS222/223/249, Anti002fix, Country_patch

--- source/usb-loader.c
+++ source/usb-loader.c
@@ -15,13 +15,21 @@
 #include "cfg.h"
 #include "fat.h"
 #include "patchcode.h"
+#include "mload/mload.h"
 
+// Prototypes
+void printcursor(u8 item, u8 citem);
+void ConfigMenu(void);
+
+// Globals
 volatile u8 GameID[] = "CRAPPY";
-volatile u8 ConfigChars[] = "CFGUSB1000000000";
+volatile u8 ConfigChars[] = "CFGUSB1000000000000";
 const char* Config = (char*)ConfigChars+6;
 s32 wbfsDev = WBFS_DEVICE_USB;
 u32 USBIOS = 249;
+u8 gIOSIndex = 0;
 
+// Used for displaying ConfigMenu
 char boottypes[2][15] = 
 	{{"USB device"},
 	{"SDHC card"}};
@@ -47,28 +55,56 @@ char languages[11][22] =
 	{"T. Chinese"},
 	{"Korean"}};
 
+char cIOS [3][4] =
+{
+    {"222"},
+    {"223"},
+    {"249"}
+};
+
+
 void printcursor(u8 item, u8 citem)
 {
 	printf("%s", (item == citem) ? "-> " : "   ");
 }
 
-void ConfigMenu()
+void ConfigMenu (void)
 {
 	u8 CurrentItem = 0;
 	s32 buttons = 0;
 	CFG.verbosemode = 1;
+
 	printf("\n*** Settings menu ***\n\n");
 	do
 	{
-		printcursor(0, CurrentItem); printf("Boot type: %s               \n", boottypes[wbfsDev-1]);
-		printcursor(1, CurrentItem); printf("Video mode: %s               \n", videos[CFG.video]);
-		printcursor(2, CurrentItem); printf("VIDTV patch: %s               \n", CFG.vidtv ? "On" : "Off");
-		printcursor(3, CurrentItem); printf("Ocarina: %s               \n", CFG.ocarina ? "On" : "Off");
-		printcursor(4, CurrentItem); printf("Language: %s               \n", languages[CFG.language]);
+		printcursor(0, CurrentItem);
+        printf("Boot type: %s               \n", boottypes[wbfsDev-1]);
+
+		printcursor(1, CurrentItem);
+        printf("Video mode: %s               \n", videos[CFG.video]);
+
+		printcursor(2, CurrentItem);
+        printf("VIDTV patch: %s               \n", CFG.vidtv ? "On" : "Off");
+
+		printcursor(3, CurrentItem);
+        printf("Ocarina: %s               \n", CFG.ocarina ? "On" : "Off");
+
+		printcursor(4, CurrentItem);
+        printf("Language: %s               \n", languages[CFG.language]);
+
+        printcursor(5, CurrentItem);
+        printf("cIOS: %s               \n", cIOS[gIOSIndex]);
+
+		printcursor(6, CurrentItem);
+        printf("Anti002 fix: %s               \n", CFG.anti_002_fix ? "On" : "Off");
+        printcursor(7, CurrentItem);
+        printf("Country patch: %s               \n", CFG.country_patch ? "On" : "Off");
+
+
 		printf("\nUse arrows to change settings\nPress A to start game\n");
 		buttons = Wpad_WaitButtons();
-		if (buttons & WPAD_BUTTON_DOWN) CurrentItem = (CurrentItem+1) % 5;
-		if (buttons & WPAD_BUTTON_UP) CurrentItem = CurrentItem ? (CurrentItem-1) : 4;
+		if (buttons & WPAD_BUTTON_DOWN) CurrentItem = (CurrentItem+1) % 8;
+		if (buttons & WPAD_BUTTON_UP) CurrentItem = CurrentItem ? (CurrentItem-1) : 7;
 		if (buttons & WPAD_BUTTON_RIGHT)
 		{
 			switch (CurrentItem)
@@ -78,6 +114,9 @@ void ConfigMenu()
 				case 2: CFG.vidtv = !CFG.vidtv; break;
 				case 3: CFG.ocarina = !CFG.ocarina; break;
 				case 4: CFG.language = (CFG.language+1) % 11; break;
+				case 5: gIOSIndex = (gIOSIndex+1) % 3; break;
+                case 6: CFG.anti_002_fix = !CFG.anti_002_fix; break;
+				case 7: CFG.country_patch = !CFG.country_patch; break;
 			}
 		}
 		if (buttons & WPAD_BUTTON_LEFT)
@@ -89,15 +128,18 @@ void ConfigMenu()
 				case 2: CFG.vidtv = !CFG.vidtv; break;
 				case 3: CFG.ocarina = !CFG.ocarina; break;
 				case 4: CFG.language = CFG.language ? CFG.language-1 : 10; break;
+				case 5: gIOSIndex = gIOSIndex ? gIOSIndex-1 : 2; break;
+                case 6: CFG.anti_002_fix = !CFG.anti_002_fix; break;
+				case 7: CFG.country_patch = !CFG.country_patch; break;
 			}
 		}
 		if (buttons & WPAD_BUTTON_HOME) Restart();
-		if (!(buttons & WPAD_BUTTON_A)) printf("\x1b[8A");
+		if (!(buttons & WPAD_BUTTON_A)) printf("\x1b[11A");
 	} while (!(buttons & WPAD_BUTTON_A));
 	printf("\n");
-}
+} // ConfigMenu
 
-
+// Main Routine
 int main(int argc, char **argv)
 {
 	s32 ret, game_num, buttons;
@@ -122,10 +164,50 @@ int main(int argc, char **argv)
 	CFG.language = ((Config[5] >= '0') && (Config[5] <= '9')) ? Config[5] - '0' : 10; // #5 language
 	wbfsDev = (Config[6] != '0') ? WBFS_DEVICE_SDHC : WBFS_DEVICE_USB; // #6 USB/SD
 	CFG.vidtv = (Config[7] != '0' ? 1 : 0); // VIDTV Patch
-	USBIOS = (Config[8] != '0' ? 222 : 249); // Use IOS222
+
+    // wiiNinja: Select from 222/223/249. Default = 249
+    switch (Config [8])
+    {
+        case '0':
+            USBIOS = 222;
+            gIOSIndex = 0;
+            break;
+
+        case '1':
+            USBIOS = 223;
+            gIOSIndex = 1;
+            break;
+
+        case '2':
+        default:
+            USBIOS = 249;
+            gIOSIndex = 2;
+            break;
+    }
 	
+    // wiiNinja: Added for the mload cIOS222/223
+	CFG.anti_002_fix = (Config[9] != '0' ? 1 : 0);  // Anti002 Fix
+	CFG.country_patch = (Config[10] != '0' ? 1 : 0); // CountryPatch
+
+    // -----------------------------------------------------------
+    // Test code only: Fake GameID here for SSBB - This works
+    // -----------------------------------------------------------
+    /*
+    //strncpy ((char *)GameID, "RSBE01", 6); // SSBB
+    strncpy ((char *)GameID, "RWSE8P", 6);  // Mario Olympics
+	wbfsDev = WBFS_DEVICE_USB;
+    USBIOS = 222;
+	CFG.ocarina = 1;
+	CFG.anti_002_fix = 1;
+	CFG.country_patch = 0;
+    CFG.video = 2;      // force video to console
+    */
+
 	/* Load Custom IOS */
 	ret = IOS_ReloadIOS(USBIOS);
+    usleep (300000);
+    if ((USBIOS == 222) || (USBIOS == 223)) // wiiNinja
+        load_ehc_module ();
 
 	/* Initialize subsystems */
 	Sys_Init();
@@ -145,18 +227,54 @@ int main(int argc, char **argv)
 	Con_Clear();
 	if (CFG.verbosemode)
 	{
-		printf("cLoader v1.3 by Cluster\n\n");
-		printf("* Hold B button to override default settings\n\n");
+		printf("cLoader v1.3 by Cluster. Mod 1.4 by wiiNinja\n\n");
+		printf("* Hold B to override default settings\n\n");
 	}
 
 	c = 0;
-   do
+    do
 	{		
 		buttons = Wpad_GetButtons();
 		c++;
 	} while ((c < 1800000) && !(buttons & WPAD_BUTTON_B) && !(buttons & WPAD_BUTTON_1));
-	if (buttons & WPAD_BUTTON_B) ConfigMenu();
-	if (buttons & WPAD_BUTTON_1) CFG.verbosemode = 1;
+	if (buttons & WPAD_BUTTON_B)
+        ConfigMenu();
+	if (buttons & WPAD_BUTTON_1)
+        CFG.verbosemode = 1;
+
+    // After ConfigMenu, check to see if user selected another cIOS
+    // If a different cIOS is selected, reload the cIOS
+    u32 userConfigIOS;
+    switch (gIOSIndex)
+    {
+        case 0:
+            userConfigIOS = 222;
+            break;
+        case 1:
+            userConfigIOS = 223;
+            break;
+        case 2:
+            userConfigIOS = 249;
+            break;
+        default:
+            userConfigIOS = USBIOS;
+            break;
+    }
+    if (USBIOS != userConfigIOS)
+    {
+        printf ("New cIOS selected. Old: %i, New: %i. Reloading...\n", USBIOS, userConfigIOS);
+		//Wpad_WaitButtons();
+
+        USBIOS = userConfigIOS;
+
+        // Reload
+        Wpad_Disconnect();
+        ret = IOS_ReloadIOS(USBIOS);
+        usleep (300000);
+        if ((USBIOS == 222) || (USBIOS == 223)) // wiiNinja
+            load_ehc_module ();
+        Wpad_Init();
+    }
 
 	/* Check if Custom IOS is loaded */
 	if (ret < 0) 
@@ -177,6 +295,8 @@ int main(int argc, char **argv)
 		{
 			printf("\r* Waiting for device... %d ", i);
 			IOS_ReloadIOS(USBIOS);
+            if ((USBIOS == 222) || (USBIOS == 223))
+                load_ehc_module ();
 			sleep(1);
 			ret = WBFS_Init(wbfsDev);
 			if(ret>=0)
@@ -223,6 +343,7 @@ int main(int argc, char **argv)
 	}
 
 	game_num = -1;
+
 	for (c = 0; c < cnt; c++)
 	{
 		if (
@@ -265,4 +386,5 @@ int main(int argc, char **argv)
 	printf("[+] ERROR: Returned! Can't boot game!");;
 	Restart_Wait();
 	return 0;
-}
+} // main
+
