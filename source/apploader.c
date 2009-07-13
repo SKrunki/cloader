338057dda0e1e87e24396bbcb3f5f3341ae0c9a3
wiiNinjaOne <wiiNinjaOne@c00eb1dc-4073-11de-af2b-f15df6ab9c44>
2009-07-13 15:56:08 +0000

    Added support for cIOS222/223. Added menu and support for Anti002fix and Country Fix. Fixed prototyping issue.

--- source/apploader.c
+++ source/apploader.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ogcsys.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "apploader.h"
 #include "wdvd.h"
@@ -29,6 +30,12 @@ static u8 *appldr = (u8 *)0x81200000;
 /* Variables */
 static u32 buffer[0x20] ATTRIBUTE_ALIGN(32);
 
+// Prototypes
+void maindolpatches(void *dst, int len);
+void PatchCountryStrings(void *Address, int Size);
+void Anti_002_fix(void *Address, int Size);
+//bool Load_Dol(void **buffer, int* dollen);
+//bool Remove_001_Protection(void *Address, int Size);
 
 static void __noprint(const char *fmt, ...)
 {
@@ -189,15 +196,9 @@ bool Search_and_patch_Video_Modes(void *Address, u32 Size, GXRModeObj* Table[])
 
 	while(Size >= sizeof(GXRModeObj))
 	{
-
-
-
 		for(i = 0; Table[i]; i+=2)
 		{
-
-
 			if(compare_videomodes(Table[i], (GXRModeObj*)Addr))
-
 			{
 				found = 1;
 				patch_videomode((GXRModeObj*)Addr, Table[i+1]);
@@ -210,7 +211,6 @@ bool Search_and_patch_Video_Modes(void *Address, u32 Size, GXRModeObj* Table[])
 		Addr += 4;
 		Size -= 4;
 	}
-
 
 	return found;
 }
@@ -257,12 +257,13 @@ s32 Apploader_Run(entry_point *entry)
 	}
 	/*HOOKS STUFF - FISHEARS*/
 
-	for (;;) {
+	for (;;)
+    {
 		void *dst = NULL;
 		s32   len = 0, offset = 0;
 
-		GXRModeObj** table = NULL;
-		/* Run apploader main function */
+		// GXRModeObj** table = NULL;
+		// Run apploader main function
 		ret = appldr_main(&dst, &len, &offset);
 		if (!ret)
 			break;
@@ -270,53 +271,8 @@ s32 Apploader_Run(entry_point *entry)
 		/* Read data from DVD */
 		WDVD_Read(dst, len, (u64)(offset << 2));
 
-		if (CFG.video == CFG_VIDEO_PATCH) // patch auto
-		{
-			switch(CONF_GetVideo())
-			{
-			case CONF_VIDEO_PAL:
-				if(CONF_GetEuRGB60() > 0) 
-				{
-					table = NTSC2PAL60;
-				}	
-				else
-				{
-					table = NTSC2PAL;
-				}
-				break;
-
-			case CONF_VIDEO_MPAL:
-				table = NTSC2PAL;
-				break;
-
-			default:
-				table = PAL2NTSC;
-				break;
-			}
-			Search_and_patch_Video_Modes(dst, len, table);
-		}
-		
-		// force PAL50 (Narolez)
-		if (CFG.video == CFG_VIDEO_PAL50) {
-			Search_and_patch_Video_Modes(dst, len, NTSC2PAL);
-		}
-		if (CFG.video == CFG_VIDEO_PAL60) {
-			Search_and_patch_Video_Modes(dst, len, NTSC2PAL60);
-		}
-		if (CFG.video == CFG_VIDEO_NTSC) {
-			Search_and_patch_Video_Modes(dst, len, PAL2NTSC);
-		}
-		
-		if (CFG.ocarina) {
-			dogamehooks(dst,len);
-		}
-		if (CFG.vidtv) {
-			vidolpatcher(dst,len);
-		}
-		/*LANGUAGE PATCH - FISHEARS*/
-		if (CFG.language != CFG_LANG_CONSOLE) {
-			langpatcher(dst,len);
-		}
+        // wiiNinja added for cIOS222/223. Code from Configurable USB Loader
+        maindolpatches(dst, len);
 
 		DCFlushRange(dst, len);
 	}
@@ -325,4 +281,247 @@ s32 Apploader_Run(entry_point *entry)
 	*entry = appldr_final();
 
 	return 0;
-}
+} // Apploader_Run
+
+void maindolpatches(void *dst, int len)
+{
+	GXRModeObj** table = NULL;
+
+	DCFlushRange(dst, len);
+	
+    if (CFG.video == CFG_VIDEO_PATCH) // patch auto
+	{
+		switch(CONF_GetVideo())
+		{
+		case CONF_VIDEO_PAL:
+			if(CONF_GetEuRGB60() > 0) 
+			{
+				table = NTSC2PAL60;
+			}	
+			else
+			{
+				table = NTSC2PAL;
+			}
+			break;
+
+		case CONF_VIDEO_MPAL:
+			table = NTSC2PAL;
+			break;
+
+		default:
+			table = PAL2NTSC;
+			break;
+		}
+		Search_and_patch_Video_Modes(dst, len, table);
+	}
+	
+	// force PAL50 (Narolez)
+	if (CFG.video == CFG_VIDEO_PAL50) {
+		Search_and_patch_Video_Modes(dst, len, NTSC2PAL);
+	}
+	if (CFG.video == CFG_VIDEO_PAL60) {
+		Search_and_patch_Video_Modes(dst, len, NTSC2PAL60);
+	}
+	if (CFG.video == CFG_VIDEO_NTSC) {
+		Search_and_patch_Video_Modes(dst, len, PAL2NTSC);
+	}
+	
+	if (CFG.ocarina) {
+		dogamehooks(dst,len);
+	}
+	if (CFG.vidtv) {
+		vidolpatcher(dst,len);
+	}
+	/*LANGUAGE PATCH - FISHEARS*/
+	if (CFG.language != CFG_LANG_CONSOLE) {
+		langpatcher(dst,len);
+	}
+	// Country Patch by WiiPower
+	if(CFG.country_patch) {
+		PatchCountryStrings(dst, len);
+	}
+
+	// 002b fix from NeoGammaR4 by WiiPower:
+	if (CFG.anti_002_fix) {
+	   	Anti_002_fix(dst, len);
+	}
+
+	DCFlushRange(dst, len);
+} // maindolpatches
+
+/*
+// wiiNinja: left here just in case we want alt.dol
+bool Load_Dol(void **buffer, int* dollen)
+{
+	int ret;
+	FILE* file;
+	void* dol_buffer;
+	
+	char fname[200];
+	char gameidbuffer4[5];
+	memset(gameidbuffer4, 0, 5);
+	memcpy(gameidbuffer4, (char*)0x80000000, 4);		
+
+	snprintf(fname, sizeof(fname), "%s/%s.dol", USBLOADER_PATH, gameidbuffer4);
+	printf("    %s\n", fname);
+
+	file = fopen( fname, "rb");
+	
+	if(file == NULL) 
+	{
+		printf("    Not Found.\n");
+		sleep(1);
+		return false;
+	}
+	
+	int filesize;
+	fseek(file, 0, SEEK_END);
+	filesize = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	//dol_buffer = malloc(filesize);
+	dol_buffer = LARGE_memalign(filesize, 32);
+	if (dol_buffer == NULL)
+	{
+		printf("Out of memory\n");
+		return false;
+	}
+	ret = fread( dol_buffer, 1, filesize, file);
+	if(ret != filesize)
+	{
+		printf("Error reading .dol");
+		free(dol_buffer);
+		fclose(file);
+		return false;
+	}
+	fclose(file);
+
+	*buffer = dol_buffer;
+	*dollen = filesize;
+	return true;
+} // Load_Dol	
+
+bool Remove_001_Protection(void *Address, int Size)
+{
+	u8 SearchPattern[16] = 	{ 0x40, 0x82, 0x00, 0x0C, 0x38, 0x60, 0x00, 0x01, 0x48, 0x00, 0x02, 0x44, 0x38, 0x61, 0x00, 0x18 };
+	u8 PatchData[16] = 		{ 0x40, 0x82, 0x00, 0x04, 0x38, 0x60, 0x00, 0x01, 0x48, 0x00, 0x02, 0x44, 0x38, 0x61, 0x00, 0x18 };
+
+	void *Addr = Address;
+	void *Addr_end = Address+Size;
+
+	while(Addr <= Addr_end-sizeof(SearchPattern))
+	{
+		if(memcmp(Addr, SearchPattern, sizeof(SearchPattern))==0) 
+		{
+			memcpy(Addr,PatchData,sizeof(PatchData));
+			return true;
+		}
+		Addr += 4;
+	}
+	return false;
+} // Remove_001_Protection
+*/
+
+
+static u8  *diskid = (u8  *)0x80000000;
+
+void PatchCountryStrings(void *Address, int Size)
+{
+    u8 SearchPattern[4]    = { 0x00, 0x00, 0x00, 0x00 };
+    u8 PatchData[4]        = { 0x00, 0x00, 0x00, 0x00 };
+    u8 *Addr            = (u8*)Address;
+
+    int wiiregion = CONF_GetRegion();
+
+    switch (wiiregion)
+    {
+        case CONF_REGION_JP:
+            SearchPattern[0] = 0x00;
+            SearchPattern[1] = 0x4A; // J
+            SearchPattern[2] = 0x50; // P
+            break;
+        case CONF_REGION_EU:
+            SearchPattern[0] = 0x02;
+            SearchPattern[1] = 0x45; // E
+            SearchPattern[2] = 0x55; // U
+            break;
+        case CONF_REGION_KR:
+            SearchPattern[0] = 0x04;
+            SearchPattern[1] = 0x4B; // K
+            SearchPattern[2] = 0x52; // R
+            break;
+        case CONF_REGION_CN:
+            SearchPattern[0] = 0x05;
+            SearchPattern[1] = 0x43; // C
+            SearchPattern[2] = 0x4E; // N
+            break;
+        case CONF_REGION_US:
+        default:
+            SearchPattern[0] = 0x01;
+            SearchPattern[1] = 0x55; // U
+            SearchPattern[2] = 0x53; // S
+    }
+
+    switch (diskid[3])
+    {
+        case 'J':
+            PatchData[1] = 0x4A; // J
+            PatchData[2] = 0x50; // P
+            break;
+
+        case 'D':
+        case 'F':
+        case 'P':
+        case 'X':
+        case 'Y':
+            PatchData[1] = 0x45; // E
+            PatchData[2] = 0x55; // U
+            break;
+
+        case 'E':
+        default:
+            PatchData[1] = 0x55; // U
+            PatchData[2] = 0x53; // S
+    }
+
+    while (Size >= 4)
+    {
+        if (Addr[0] == SearchPattern[0] && Addr[1] == SearchPattern[1] && Addr[2] == SearchPattern[2] && Addr[3] == SearchPattern[3])
+        {
+            //*Addr = PatchData[0];
+            Addr += 1;
+            *Addr = PatchData[1];
+            Addr += 1;
+            *Addr = PatchData[2];
+            Addr += 1;
+            //*Addr = PatchData[3];
+            Addr += 1;
+            Size -= 4;
+        } else
+        {
+            Addr += 4;
+            Size -= 4;
+        }
+    }
+} // PatchCountryStrings
+
+
+
+void Anti_002_fix(void *Address, int Size)
+{
+	u8 SearchPattern[12] = 	{ 0x2C, 0x00, 0x00, 0x00, 0x48, 0x00, 0x02, 0x14, 0x3C, 0x60, 0x80, 0x00 };
+	u8 PatchData[12] = 		{ 0x2C, 0x00, 0x00, 0x00, 0x40, 0x82, 0x02, 0x14, 0x3C, 0x60, 0x80, 0x00 };
+
+	void *Addr = Address;
+	void *Addr_end = Address+Size;
+
+	while(Addr <= Addr_end-sizeof(SearchPattern))
+	{
+		if(memcmp(Addr, SearchPattern, sizeof(SearchPattern))==0) 
+		{
+			memcpy(Addr,PatchData,sizeof(PatchData));
+		}
+		Addr += 4;
+	}
+} // Anti_002_fix
+
